bcec6dfebcc2699d42f2db8da70385f025080186
HoangHuuToan <109668328+HoangHuuToan@users.noreply.github.com>
2023-03-20 23:12:51 +0700

    update connect DB

---
+++ Example1.API/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Example1.API.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Guid NumberPhone { get; set; }
+        public string ImageAvt { get; set; }
+        public int Role { get; set; }
+
+    }
+}
---
+++ Example1API/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace Example1API.Entities
+{
+    public class User
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string NumberPhone { get; set; }
+        public string Role { get; set; }
+
+    }
+}
