29ad62b16490950c30f775848855feb586b7900d
HoangHuuToan <109668328+HoangHuuToan@users.noreply.github.com>
2023-03-21 17:31:21 +0700

    update

---
+++ Example01/Data/User.cs
@@ -0,0 +1,23 @@
+﻿namespace Example01.Data
+{
+    public class User
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string NumberPhone { get; set; }
+        public string Role { get; set; }
+        public User(int id , string name , string email, string numberphone, string role) {
+        
+            Id = id;
+            Name = name;
+            Email = email;
+            NumberPhone = numberphone;
+            Role = role;
+        }
+        public User()
+        {
+        }
+
+    }
+}
